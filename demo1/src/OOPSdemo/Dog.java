9797f09ca4713a3d3765bdd8a887221383f04da6
pashapaladin <82773893+pashapaladin@users.noreply.github.com>
2024-01-31 14:15:05 -0600

    Update Dog.java

--- demo1/src/OOPSdemo/Dog.java
+++ demo1/src/OOPSdemo/Dog.java
@@ -2,25 +2,27 @@ package OOPSdemo;
 
 
 public class Dog {
-	int leg =4;
-	int eye = 2;
-	String name = "tommy";
-	String color="white";
-	
+	int leg;
+	int eye;
+	String color;
+	String breed;
+	String favorite_food;	
+	String owner;
 	// methods here can be accessed by other files.
 	public void run() {
 		System.out.println("my dog runs");}
 
+
+	public class Dog(){}
+	public class Dog(leg, eye, name, color){}
+	public class Dog(breed, favorite_food, owner){}	
+	public class Dog(size, run_speed,jump_height){}	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog Poppy = new Dog(4,2,"gold");
+	
+	}
+
 }
 
-
-// create object
-
-//public class OOPStest1 {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
