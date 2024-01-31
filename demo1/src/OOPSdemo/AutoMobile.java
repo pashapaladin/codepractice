932e4e835c310faa5280fa3c8f978e8f962277a3
pashapaladin <82773893+pashapaladin@users.noreply.github.com>
2024-01-31 14:20:50 -0600

    Update AutoMobile.java

--- demo1/src/OOPSdemo/AutoMobile.java
+++ demo1/src/OOPSdemo/AutoMobile.java
@@ -2,21 +2,23 @@ package OOPSdemo;
 
 
 public class AutoMobile {
-  String bodytype;
-	String engine;
-	String color;
+	String color="red";
+	int wheels;
+	int model_num;
+	// String bodytype;
+	// String engine;
 
   public class AutoMobile(){}
-  public class AutoMobile(bodytype, engine, color){}
-
-	// methods here can be accessed by other files.
-	public void engineStart() {
+  // public class AutoMobile(bodytype, engine, color){}
+  public class AutoMobile(color, wheels, model_num){}
+  // methods here can be accessed by other files.
+  public void engineStart() {
 		System.out.println("car is started");
   }
   public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		AutoMobile Bugatti = new AutoMobile("sportscar","W16","Red");
-
+		// AutoMobile Bugatti = new AutoMobile("sportscar","W16","Red");
+		AutoMobile Huracan = new AutoMobile;
 	}
 
 }
