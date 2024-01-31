cc19cc1b66a843d11a6dc4e9f276ded22b557437
pashapaladin <82773893+pashapaladin@users.noreply.github.com>
2024-01-31 09:34:29 -0600

    Create VotingAge.java

---
+++ demo1/src/demo1/VotingAge.java
@@ -0,0 +1,18 @@
+package demo1;
+
+public class VotingAge {
+  static bool checkAge(int age) {
+    if (age>17){
+      return true;}
+    else {
+      return false;}
+  }
+
+
+  public static void main(String[] args) {
+  		checkAge(25);
+  		/*
+  		 * for(int i=0; i<20;i++) { System.out.println(i); }
+  		 */
+  }
+}
